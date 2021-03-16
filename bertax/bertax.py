c6080e1a8f515610e62d2b5d27f217d2adb3f653
Fleming Kretschmer <fleming.kretschmer@uni-jena.de>
2021-03-16 14:54:12 +0100

    better formatted help, disabled TF warnings output

--- bertax/bertax.py
+++ bertax/bertax.py
@@ -1,6 +1,11 @@
+import warnings
+warnings.simplefilter(action='ignore', category=FutureWarning)
+import os
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '1'
 import argparse
 from bertax.utils import (seq2tokens, get_token_dict, process_bert_tokens_batch, load_bert,
                           parse_fasta, annotate_predictions, best_predictions, seq_frames)
+import tensorflow as tf
 from logging import  info, getLogger, INFO, WARNING
 import numpy as np
 from random import sample
@@ -8,23 +13,29 @@ import json
 import pkg_resources
 
 def parse_arguments(argv=None):
-    parser = argparse.ArgumentParser(description='BERTax: Predicting sequence taxonomy')
+    parser = argparse.ArgumentParser(prog='bertax',
+                                     description='BERTax: Predicting sequence taxonomy',
+                                     formatter_class=lambda prog: argparse.HelpFormatter(prog,max_help_position=42))
     parser.add_argument('fasta')
     parser.add_argument('-v', '--verbose', help='more verbose output if set, including progress bars',
                         action='store_true')
     parser.add_argument('-o', '--output_file', help='write output to specified file (Default: stdout)',
-                        default=None)
-    parser.add_argument('--conf_matrix_file', help='if set, writes confidences for all possible classes in all ranks to specified file (JSON)',
-                        default=None)
-    parser.add_argument('--long_sequence_behavior', help='how to handle sequences longer than the maximum size: '
-                        'split to equal chunks, use all possible sequence frames, use random sequence window; '
-                        'for multiple chunks, the mean is calculated (Default: equal_chunks)',
-                        choices=['equal_chunks', 'all_frames', 'window'], default='equal_chunks')
+                        default=None, metavar='FILE')
+    parser.add_argument('--conf_matrix_file', help='if set, writes confidences for all possible classes '
+                        'in all ranks to specified file (JSON)', default=None, metavar='FILE')
+    parser.add_argument('--long_sequence_behavior', help='how to handle sequences longer '
+                        'than the maximum size: ' 'split to equal chunks (`equal chunks`), '
+                        'use all possible sequence frames (`all_frames`), use random '
+                        'sequence window (`window`); for multiple chunks, the mean is '
+                        'calculated (Default: equal_chunks)',
+                        choices=['equal_chunks', 'all_frames', 'window'],
+                        default='equal_chunks',
+                        metavar='BEHAVIOR')
     parser.add_argument('-C', '--maximum_sequence_chunks', default=10, type=int,
-                        help='maximum number of chunks to use for long sequences')
+                        help='maximum number of chunks to use for long sequences', metavar='NR')
     parser.add_argument('--output_ranks', help='rank predictions to include in output '
                         '(Default: superkingdom phylum genus)',
-                        default=['superkingdom', 'phylum', 'genus'], nargs='+')
+                        default=['superkingdom', 'phylum', 'genus'], nargs='+', metavar='RANK')
     parser.add_argument('--no_confidence', action='store_true',
                         help='do not include class confidence values in output')
     parser.add_argument('--batch_size', type=int, help='batch size for predictions (Default: 32)',
